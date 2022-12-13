7b4b264effff9b007fba7d51844af337e5581267
Kelve Rabelo <kelve.r.farias@hotmail.com>
2022-12-13 11:02:35 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,28 +1,9 @@
-let toggleBtn = document.getElementById('theme-btn');
-let body = document.body;
-let darkMode = localStorage.getItem('dark-mode');
+function changeColor() {
+   let tema = document.querySelector("#theme-btn")
 
-const enableDarkMode = () =>{
-   toggleBtn.classList.replace('bi-sun-fill', 'bi-moon-fill');
-   body.classList.add('dark');
-   localStorage.setItem('dark-mode', 'enabled');
-}
+   if (className == "active") {
+      alert("confere!")
 
-const disableDarkMode = () =>{
-   toggleBtn.classList.replace('bi-moon-fill', 'bi-sun-fill');
-   body.classList.remove('dark');
-   localStorage.setItem('dark-mode', 'disabled');
-}
-
-if(darkMode === 'enabled'){
-   enableDarkMode();
-}
-
-toggleBtn.onclick = (e) =>{
-   darkMode = localStorage.getItem('dark-mode');
-   if(darkMode === 'disabled'){
-      enableDarkMode();
-   }else{
-      disableDarkMode();
    }
 }
+changeColor()
