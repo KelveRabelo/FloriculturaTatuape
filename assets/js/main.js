e8817b0802c1725501973d48bffceddd641250b8
Kelve Rabelo <kelve.r.farias@hotmail.com>
2023-03-08 17:30:28 -0300

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+<<<<<<< HEAD
+//=================> CHECK DOCUMENT WAS LOADING <=================//
+if(document.readyState == "loading")
+{
+    document.addEventListener("DOMContentLoaded", update)
+}
+else
+{
+    update()
+=======
 //========================> CHECK HTML <==========================//
 //Check elements document was loading
 if (document.readyState == "loading") {
@@ -5,9 +15,36 @@ if (document.readyState == "loading") {
 }
 else {
     ready()
+>>>>>>> 4de294eb40b2bcfa098d2577abaffabcfaebd37f
 }
-//==================================================================
 
+<<<<<<< HEAD
+//=================> VARIABLES GLOBALS <=================//
+const hamburguer        = document.querySelector("#hamburguer")
+const linkMenu          = document.querySelectorAll(".linkMenu")
+const cart              = document.querySelector(".header-cart")
+const btnCart           = document.querySelectorAll(".btnCart")
+const btnCloseCart      = document.querySelectorAll(".btnCloseCart")
+const cartHistory       = document.querySelector(".header-cart-history")
+const btnHistory        = document.querySelector(".btnHistory")
+const removeCart        = document.querySelectorAll(".removeCart")
+const inputUpdateQuantity = document.getElementsByClassName("quantityProductCart")
+const btnBuy            = document.querySelector(".btnBuy");
+const notification      = document.querySelector(".notifications");
+const userName          = document.querySelector("#userName")
+const userNumber        = document.querySelector("#userNumber")
+const userPayment       =   document.querySelector("#userPayment")
+
+
+//=================> FUNCTION UPDATE <=================//
+function update()
+{
+    renderCartProductsHistory()
+    renderCartProducts()
+    renderProducts()
+    updateCartTotal()
+    menu()
+=======
 //Vabriables global
 var totalAmount = "0,00"
 var nPedido = 1
@@ -38,11 +75,25 @@ function ready() {
     const btnCloseCartHistory = document.querySelector("#cart-close-history")
     const btnCart = document.querySelector("#btn-cart-history")
 
+>>>>>>> 4de294eb40b2bcfa098d2577abaffabcfaebd37f
 
-    ///////////////////////////// MENU /////////////////////////////
+}
+//=================> MENU <=================//
+function menu()
+{
+    //HAMBUGER
     hamburguer.addEventListener("click", () => {
         cart.classList.remove("active")
         cartHistory.classList.remove("active")
+<<<<<<< HEAD
+    }) 
+
+    for(i = 0; i < linkMenu.length; i++) 
+    {
+        linkMenu[i].addEventListener("click", () => {
+            if (hamburguer.checked) {
+                hamburguer.checked = false
+=======
     })
 
     //hidden menu at click in link
@@ -50,10 +101,13 @@ function ready() {
         menuLinks[i].addEventListener("click", () => {
             if (document.querySelector(".header-hamburguer").checked) {
                 document.querySelector(".header-hamburguer").checked = false
+>>>>>>> 4de294eb40b2bcfa098d2577abaffabcfaebd37f
             }
-            console.log(menuLinks[i])
         })
     }
+<<<<<<< HEAD
+    ////////////////////////////////////////////////////
+=======
 
     /////////////////////////////// CART ////////////////////////////
     //open cart
@@ -67,10 +121,67 @@ function ready() {
         cart.classList.remove("active")
         cartHistory.classList.remove("active")
     })
+>>>>>>> 4de294eb40b2bcfa098d2577abaffabcfaebd37f
 
-    //open history
-    btnOpenCartHistory.addEventListener("click", () => {
+   //OPEN CART
+   for (i=0; i < btnCart.length; i++)
+   {
+        btnCart[i].addEventListener("click", () => {
+            cart.classList.add("active")
+            cartHistory.classList.remove("active")
+            hamburguer.checked = false
+        })
+   }
+  
+    //CLOSE CART
+    for(i=0; i < btnCloseCart.length; i++)
+    {
+        btnCloseCart[i].addEventListener("click", () => {
+            cart.classList.remove("active")
+            cartHistory.classList.remove("active")
+        })
+    
+    }
+    
+    //OPEN HISTORY CART
+    btnHistory.addEventListener("click", () => {
+        cart.classList.remove("active")
         cartHistory.classList.add("active")
+<<<<<<< HEAD
+    })
+
+}
+//=================> NOTIFICATIONS <=================//
+function removeNotification()
+{
+    notification.classList.remove("active");
+}
+function notificationAdd()
+{
+  
+    notification.innerHTML = `<p>item adicionado ao carrinho =)</p>`;
+    notification.classList.add("active");
+    setTimeout(removeNotification, 2000)
+}
+function notificationRemove()
+{
+    notification.innerHTML = `<p>item removido do carrinho =(</p>`
+    notification.classList.add("active")
+    setTimeout(removeNotification, 2000)
+}
+//=================> FUNÇÃO RENDER PRODUCTS CONTAINER <=================//
+function renderProducts()
+{
+   const productsRender = document.querySelector(".containerProducts")
+   productsRender.innerHTML = ""
+    for (item in products)
+    {
+        productsRender.innerHTML += 
+        `
+            <div class="cardProduct">
+            	<img src=${products[item].imgSrc} alt="" class="imgProduct">
+                <h2 class="nameProduct">${products[item].name}</h2>
+=======
         cart.classList.remove("active")
         document.querySelector(".header-hamburguer").checked = false
     })
@@ -523,12 +634,273 @@ function renderProdcuts() {
             <div class="product-box">
             	<img src=${products[i].imgSrc} alt="" class="product-img">
                 <h2 class="product-title">${products[i].name}</h2>
+>>>>>>> 4de294eb40b2bcfa098d2577abaffabcfaebd37f
                 <div class="detail">
-                	<span class="product-price">R$${products[i].price.toFixed(2).replaceAll(".", ",")}</span>
-			        <i class="bi bi-bag-plus-fill add-cart"></i>  
+                	<span class="priceProduct">R$${products[item].price.toFixed(2).replaceAll(".", ",")}</span>
+                    <i id="${products[item].id}" class="bi bi-bag-plus-fill btnAddCart" onclick="addProductCart(this)"></i>
             	</div>
+        
             </div>
         `
     }
 }
+//=================> FUNÇÃO RENDER PRODUCTS CART <=================//
+function renderCartProducts()
+{
+    const cartProductsList = JSON.parse(localStorage.getItem('cartProductsList')) || [];
+    
+    const newNode = document.querySelector('.contentCart');
+    
+    // Clear existing content
+    newNode.innerHTML = '';
+    
+    // Check if cart is empty
+    if (cartProductsList.length === 0) {
+        newNode.innerHTML = `
+        <p class="nullCart">Seu carrinho está vazio!</p>
+        <p class="nullCart">Faça um pedido.</p>
+        `;
+        return;
+    }
+    
+    // Render cart products
+    cartProductsList.forEach((product) => {
+        newNode.innerHTML += `
+            <div class="cardCart" data-id="${product.id}">
+                <img src="${product.img}" alt="${product.name}">
+                <div class="cardDetail">
+                    <div class="nameProductCart">${product.name}</div>
+                    <div class="priceProductCart">$${product.price.toFixed(2).replace(".", ",")}</div>
+                    <input type="number" value="${product.quantity}" class="quantityProductCart" onchange="inputUpdateProduct()" min="1">
+                    </div>
+                <button class="removeProductCart bi bi-trash3-fill"></button>
+            </div>
+        `;
+    });
+    
+    // Add event listener for remove button
+    document.querySelectorAll('.removeProductCart').forEach((button) => {
+        button.addEventListener('click', removeProductCart);
+    });
+}
+function renderCartProductsHistory()
+{
+    const cartProductsListHistory = JSON.parse(localStorage.getItem("cartProductsListHistory")) || [];
+
+    // atualizando o conteúdo do elemento ".cardHistory"
+    const cartProductsHistory = document.querySelector(".cardHistory");
+    cartProductsHistory.innerHTML = "";
+  
+    cartProductsListHistory.forEach((pedido) => {
+      // cria um loop para renderizar cada produto do pedido
+      let produtosHtml = "";
+      pedido.produtos.forEach((produto) => {
+        produtosHtml += `
+          <div class="bodyCardHistory">
+            <img src=${produto.img} alt="">
+            <p>${produto.name}</p>
+            <p>${produto.quantity}x</p>
+            <p>R$${produto.price.toFixed(2).replace(".", ",")}</p>
+          </div>
+        `;
+      });
+  
+      cartProductsHistory.innerHTML += `
+        <div class="pedidoCardHistory">
+          <div class="headerCardHistory">
+            <p>Nº Pedido: <span>${pedido.order}</span></p>
+            <p>Data: <span>${pedido.dataHora}</span></p>
+          </div>
+          ${produtosHtml}
+          <div class="footerCardHistory">
+            <p>Total: </p>
+            <p>R$${pedido.total.toFixed(2).replace(".", ",")}</p>
+          </div>
+        </div>
+      `;
+    });
+  
+}
+//=================> FUNÇÃO ADD PRODUCT TO CART <=================//
+function addProductCart(id)
+{
+    // Obter a lista de produtos no carrinho da localStorage ou criar uma lista vazia
+    const cartProductsList = JSON.parse(localStorage.getItem('cartProductsList')) || [];
+  
+    // Criar objeto do produto selecionado
+    let product;
+    for (item in products) {
+      if (id.id == products[item].id) {
+        product = {
+          id: products[item].id,
+          name: products[item].name,
+          price: products[item].price,
+          quantity: products[item].quantity,
+          img: products[item].imgSrc,
+        };
+      }
+    }
+  
+    // Verificar se o produto já existe no carrinho
+    const existingProductIndex = cartProductsList.findIndex((p) => p.id === product.id);
+  
+    // Se o produto já existir, atualizar a quantidade
+    if (existingProductIndex !== -1) {
+      cartProductsList[existingProductIndex].quantity += product.quantity;
+    } 
+    // Se o produto não existir, adicionar à lista de produtos no carrinho
+    else {
+        cartProductsList.push(product);
+        //Notication add product
+        notificationAdd()
+      
+    }
+  
+    // Salvar a lista atualizada de produtos no carrinho na localStorage
+    localStorage.setItem('cartProductsList', JSON.stringify(cartProductsList));
+
+    
+    update()
+}
+//=================> FUNÇÃO REMOVE PRODUCT TO CART <=================//
+function removeProductCart(event)
+{
+    
+    const cartProductsList = JSON.parse(localStorage.getItem('cartProductsList')) || [];
+
+    const buttonClicked = event.target;
+    const cardToRemove = buttonClicked.closest('.cardCart');
+    const productId = cardToRemove.dataset.id;
+  
+    // Remove from cart
+    cartProductsList.splice(
+      cartProductsList.findIndex((p) => p.id === productId),
+      1
+    );
+   
+    //Notification Remove
+    notificationRemove()
+
+    // Update LocalStorage
+    localStorage.setItem("cartProductsList", JSON.stringify(cartProductsList));
+  
+    // Remove from HTML
+    cardToRemove.remove();
+
+    update()
+}
+//=================> FUNÇÃO UPDATE TOTAL CART <=================//
+function updateCartTotal()
+{
+    const cartProductsList = JSON.parse(localStorage.getItem('cartProductsList')) || [];
+    const totalElement = document.querySelector('.total-price');
+    let total = 0;
+  
+    cartProductsList.forEach((product) => {
+      total += product.price * product.quantity;
+    });
+  
+    totalElement.innerHTML = `R$${total.toFixed(2).replace('.', ',')}`;
+
+    localStorage.setItem("cartTotal", total.toFixed(2));
+}
+//=================> FUNÇÃO UPDATE QUANTITY CART <=================//
+function inputUpdateProduct()
+{
+    // Obter a lista de produtos no carrinho da localStorage ou criar uma lista vazia
+    const cartProductsList = JSON.parse(localStorage.getItem('cartProductsList')) || [];
+
+    let index = document.getElementsByClassName("cardCart")
+
+    for(let i=0; i < inputUpdateQuantity.length; i++)
+    {
+        if(inputUpdateQuantity[i].value <= 0)
+        {
+            index = index[i].getAttribute("data-id")
+            inputUpdateQuantity[i].value = 1
+            /* console.log("index: ",index)
+            console.log("quantity: ", quantity) */
+        }
+        else
+        {
+            let index = document.getElementsByClassName("cardCart")
+            index = index[i].getAttribute("data-id")
+            let quantity = inputUpdateQuantity[i].value 
+            cartProductsList[i].quantity = parseInt(quantity)
+        }
+    } 
+    localStorage.setItem('cartProductsList', JSON.stringify(cartProductsList));
+    update()
+}
+
+//=================> CHECKOUT WHATSAPP <=================//
+btnBuy.addEventListener("click", checkout)
+
+function checkout() {
+    let cartProductsListHistory = JSON.parse(localStorage.getItem("cartProductsListHistory")) || [];
+    let cartProductsList = JSON.parse(localStorage.getItem('cartProductsList')) || [];
+    let cartTotal = JSON.parse(localStorage.getItem("cartTotal")) || 0;
+  
+    let prefix = "FT"
+    let random = Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000 // número aleatório para simular um número de pedido
+    let order = prefix + random
+    // criando o objeto que representa o pedido
+    const pedido = {
+      order: order, 
+      data: new Date().toLocaleString().split(',')[0], // data e hora atual formatada como string
+      produtos: cartProductsList, // lista de produtos do carrinho
+      total: cartTotal, // valor total dos produtos do carrinho
+    };
+    // adicionando o pedido ao histórico de pedidos
+    cartProductsListHistory.push(pedido);
+
+    let productListText = cartProductsList
+    .map(item => `| ${item.name.padEnd(22)} ${item.quantity.toString()}x ${"".padEnd(18)}R$${item.price.toFixed(2)}`).join("%0A");
+
+    let message =
+    "%2B+---------------------------------------------%2B+" +
+    "%0A|               ❁ Floricultura Tatuapé ❁              |" +
+    "%0A%2B+---------------------------------------------%2B+" +
+    "%0A| Dados do cliente                                         |" +
+    "%0A%2B+---------------------------------------------%2B+" +
+    `%0A| Nome: ${userName.value.toString().padEnd(53)}|` +
+    `%0A| Número: ${userNumber.value.padEnd(42)}|` +
+    `%0A| Pagamento: ${userPayment.value.padEnd(46)}|` +
+    `%0A| Order: ${pedido.order.toString().padEnd(50)}|` +
+    `%0A| Data: ${pedido.data.padEnd(50)}|` +
+    "%0A%2B+---------------------------------------------%2B+" +
+    "%0A|                        Lista de produtos                 |" +
+    "%0A%2B+---------------------------------------------%2B+" +
+    `%0A| Nome${"".padEnd(15)}Qtd${"".padEnd(20)}Preço${"".padEnd(8)}|`+  
+    `%0A${productListText}` +
+    "%0A%2B+---------------------------------------------%2B+" +
+    `%0A| Total: R$ ${cartTotal.toFixed(2).toString().padEnd(49)}|` +
+    "%0A%2B+---------------------------------------------%2B+";
+
+    let url = "https://wa.me/5511953604803?text=" + message;
+
+    userName.value = ""
+    userNumber.value = ""
+    userPayment.value = ""
+
+    window.open(url, "_blank").focus();
+  
+
+    // limpando o carrinho de compras
+    cartProductsList = [];
+    cartTotal = 0;
+    localStorage.setItem("cartProductsList", JSON.stringify(cartProductsList));
+    localStorage.setItem("cartTotal", JSON.stringify(cartTotal));
+    localStorage.setItem("cartProductsListHistory", JSON.stringify(cartProductsListHistory));
+
+    update();
+}
+
+
+//======================> FUNÇÃO VALIDADE FORM <=====================//
+
 //==================================================================
+
+
+// Call renderCartProducts on page load
+window.addEventListener('load', renderCartProducts);
--- assets/js/products.js
+++ assets/js/products.js
@@ -4,70 +4,71 @@ const products =
         id: 0,
         name: "amarílis",
         price: 25.00,
-        discount: 10,
+        quantity: 1,
         imgSrc: "assets/img/flor.jpg"
     },
     {
         id: 1,
         name: "azaleia",
         price: 79.00,
-        discount: 15,
+        quantity: 1,
         imgSrc: "assets/img/flor.jpg"
     },
     {
         id: 2,
         name: "camélia",
         price: 49.00,
-        discount: 10,
+        quantity: 1,
         imgSrc: "assets/img/flor.jpg"
     },
     {
         id: 3,
         name: "cravina",
         price: 29.90,
-        discount: 5,
+        quantity: 1,
         imgSrc: "assets/img/flor.jpg"
     },
     {
         id: 4,
         name: "crisântemo",
         price: 39.90,
-        discount: 5,
+        quantity: 1,
         imgSrc: "assets/img/flor.jpg"
     },
     {
         id: 5,
         name: "gérbera",
         price: 49.00,
-        discount: 5,
+        quantity: 1,
         imgSrc: "assets/img/flor.jpg"
     },
     {
         id: 6,
         name: "hortênsia",
         price: 60.00,
-        discount: 5,
+        quantity: 1,
         imgSrc: "assets/img/flor.jpg"
     },
     {
         id: 7,
         name: "hibisco",
         price: 39.90,
-        discount: 9,
+        quantity: 1,
         imgSrc: "assets/img/flor.jpg"
     },
     {
         id: 8,
         name: "tulipa",
         price: 29.90,
-        discount: 9,
+        quantity: 1,
         imgSrc: "assets/img/flor.jpg"
     },
     {
         id: 9,
         name: "rosa",
         price: 19.90,
-        discount: 9,
+        quantity: 1,
         imgSrc: "assets/img/flor.jpg"
     }
 ]
+
